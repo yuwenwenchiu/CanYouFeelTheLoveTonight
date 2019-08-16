598999f57b86a47e41504b77d44b68bb6cba2367
yuwen <repunzel0316@gmail.com>
2019-08-16 14:27:29 +0800

    version2：add model

---
+++ CanYouFeelTheLoveTonight/Model/Lyrics.swift
@@ -0,0 +1,17 @@
+//
+//  Lyrics.swift
+//  CanYouFeelTheLoveTonight
+//
+//  Created by Yuwen Chiu on 2019/8/12.
+//  Copyright © 2019 YuwenChiu. All rights reserved.
+//
+
+import Foundation
+
+// 建立歌詞類別
+struct Lyrics {
+    
+    let text: String
+    let name: String
+    
+}
